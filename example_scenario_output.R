73e65fc9370a6020ae6591e4587ee4abbddcebf0
Li Shandross <57642277+lshandross@users.noreply.github.com>
2024-03-07 10:20:49 -0500

    Changes to ensure manuscript renders properly

--- example_scenario_output.R
+++ example_scenario_output.R
@@ -4,6 +4,7 @@
 
 library(hubData)
 hub_path <- "../example-complex-scenario-hub"
+here::here("analysis/data/raw_data/flu_truth_all.rds")
 example_scenario_output <- hubData::connect_hub(hub_path) |>
   dplyr::collect() |>
   dplyr::select(model_id, origin_date, location, horizon, target, scenario_id, output_type, output_type_id, value) |>
@@ -14,5 +15,5 @@ example_scenario_output <- hubData::connect_hub(hub_path) |>
                 output_type_id %in% c(0.25, 0.5, 0.75),
                 model_id == "HUBuni-simexamp")
 
-saveRDS(example_scenario_output, file = "analysis/data/example_data/example_scenario_output.rds")
+# saveRDS(example_scenario_output, file = "analysis/data/example_data/example_scenario_output.rds")
 
